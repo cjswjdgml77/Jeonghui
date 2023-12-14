c2419eae69857ed6c3ee800991d132d48821c8d3
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-12-15 08:31:19 +1100

    style : Add Rale fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        Raleway: ['"Raleway"', "serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
