d9f3b2cd6663e35601f71fbcbf28803b07eb1c3b
JeonghuiCheon <cjswjdgml77@gmail.com>
2023-12-15 23:00:54 +1100

    style : unable mouse select

--- src/2d/components/StickyContainer.tsx
+++ src/2d/components/StickyContainer.tsx
@@ -1,7 +1,11 @@
 import { PropsWithChildren } from "react";
 
 const StickyContainer = ({ children }: PropsWithChildren) => {
-  return <div className="w-full h-full absolute top-0 left-0 ">{children}</div>;
+  return (
+    <div className="w-full h-full absolute top-0 left-0 pointer-events-none">
+      {children}
+    </div>
+  );
 };
 
 export default StickyContainer;
